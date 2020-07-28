7edab2cca17222d634eee5044ccf22f4079500ea
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2020-07-28 17:09:54 +0300

    Add files via upload

---
+++ Citizen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _cildren;
+        public readonly int _fatherID;
+        public readonly int _iD;
+        public static int _numberOfCurrentCitizens;
+        public const int _MAXIMUM_NUMBER_OF_CITIZENS = 45000;
+
+        public Citizen(string name,int fatherID)
+        {
+            _name = name;
+            _fatherID = fatherID;
+        }
+        public static void PrintNumberOfCurrentCitizens()
+        {
+            Console.WriteLine(_numberOfCurrentCitizens);
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine("$ The ID is {iD}");
+        }
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            int Gap;
+            Gap = _fatherID - _iD;
+            Console.WriteLine("$ The Gap is {Gap}");
+        }
+        public void SetChildren(Citizen[] citizens)
+        {
+            
+        }
+
+    }
+}
